0a333374e84d4ca4fe40977fde055a3545295326
bossm0n5t3r <bossm0n5t3r@gmail.com>
2023-06-09 01:00:00 +0900

    feat: Add OS, Architecture enums

---
+++ src/main/kotlin/redis/embedded/enums/Architecture.kt
@@ -0,0 +1,8 @@
+package redis.embedded.enums
+
+enum class Architecture {
+    X86,
+    X86_64,
+    ARM64,
+    UNKNOWN,
+}
---
+++ src/main/kotlin/redis/embedded/enums/OS.kt
@@ -0,0 +1,10 @@
+package redis.embedded.enums
+
+enum class OS {
+    UNIX,
+    WINDOWS,
+    MAC_OS_X,
+    SOLARIS,
+    FREE_BSD,
+    UNKNOWN,
+}
