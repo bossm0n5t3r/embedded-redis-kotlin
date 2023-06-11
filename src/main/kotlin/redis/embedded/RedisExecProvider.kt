82c0ccdd6932f76720b963b8e2cac752f52a5ad7
bossm0n5t3r <bossm0n5t3r@gmail.com>
2023-06-12 01:00:05 +0900

    feat: Add RedisExecProvider

---
+++ src/main/kotlin/redis/embedded/RedisExecProvider.kt
@@ -0,0 +1,62 @@
+package redis.embedded
+
+import com.google.common.annotations.VisibleForTesting
+import redis.embedded.enums.Architecture
+import redis.embedded.enums.OS
+import redis.embedded.utils.JarUtil
+import java.io.File
+import java.io.IOException
+
+class RedisExecProvider {
+    companion object {
+        const val REDIS_VERSION = "0.0.1"
+
+        fun defaultProvider(): RedisExecProvider {
+            return RedisExecProvider()
+        }
+    }
+    private val executables = mutableMapOf<OsArchitecture, String>()
+
+    init {
+        initExecutables()
+    }
+
+    private fun initExecutables() {
+        executables[OsArchitecture.UNIX_X86] = "redis-server-$REDIS_VERSION-linux-386"
+        executables[OsArchitecture.UNIX_X86_64] = "redis-server-$REDIS_VERSION-linux-amd64"
+        executables[OsArchitecture.UNIX_ARM64] = "redis-server-$REDIS_VERSION-linux-arm64"
+        executables[OsArchitecture.MAC_OS_X_X86_64] = "redis-server-$REDIS_VERSION-darwin-amd64"
+        executables[OsArchitecture.MAC_OS_X_ARM64] = "redis-server-$REDIS_VERSION-darwin-arm64"
+    }
+
+    @VisibleForTesting
+    fun override(os: OS, executable: String): RedisExecProvider {
+        for (arch in Architecture.values()) {
+            override(os, arch, executable)
+        }
+        return this
+    }
+
+    @VisibleForTesting
+    fun override(os: OS, arch: Architecture, executable: String): RedisExecProvider {
+        executables[OsArchitecture(os, arch)] = executable
+        return this
+    }
+
+    @Throws(IOException::class)
+    fun get(): File {
+        val osArch = OsArchitecture.detect()
+        require(executables.containsKey(osArch)) { "No Redis executable found for $osArch" }
+        val executablePath = executables[osArch]
+        requireNotNull(executablePath) { "No Redis executablePath found for $osArch" }
+        return if (fileExists(executablePath)) {
+            File(executablePath)
+        } else {
+            JarUtil.extractExecutableFromJar(executablePath)
+        }
+    }
+
+    private fun fileExists(executablePath: String): Boolean {
+        return File(executablePath).exists()
+    }
+}
