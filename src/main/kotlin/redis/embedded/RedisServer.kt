4640767bc3b386e7d97d4b18fca71d34244c1957
bossm0n5t3r <bossm0n5t3r@gmail.com>
2023-06-12 01:09:21 +0900

    feat: Add RedisServer

---
+++ src/main/kotlin/redis/embedded/RedisServer.kt
@@ -0,0 +1,46 @@
+package redis.embedded
+
+import java.io.File
+
+class RedisServer : AbstractRedisInstance {
+    @JvmOverloads
+    constructor(port: Int = DEFAULT_REDIS_PORT) : super(port) {
+        args = builder().port(port).build().args
+    }
+
+    constructor(port: Int, tlsPort: Int) : super(port, tlsPort) {
+        args = builder().port(port).tlsPort(tlsPort).build().args
+    }
+
+    constructor(executable: File, port: Int) : super(port) {
+        args = mutableListOf(
+            executable.absolutePath,
+            "--port",
+            port.toString(),
+        )
+    }
+
+    constructor(redisExecProvider: RedisExecProvider, port: Int) : super(port) {
+        args = mutableListOf(
+            redisExecProvider.get().absolutePath,
+            "--port",
+            port.toString(),
+        )
+    }
+
+    internal constructor(args: List<String>, port: Int, tlsPort: Int) : super(port, tlsPort) {
+        this.args = args.toMutableList()
+    }
+
+    override fun redisReadyPattern(): String {
+        return REDIS_READY_PATTERN
+    }
+
+    companion object {
+        private const val REDIS_READY_PATTERN = ".*(R|r)eady to accept connections.*"
+        const val DEFAULT_REDIS_PORT = 6379
+        fun builder(): RedisServerBuilder {
+            return RedisServerBuilder()
+        }
+    }
+}
