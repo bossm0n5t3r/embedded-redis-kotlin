184af00bc96f5654db1a88d0faec6729e30a4cb9
bossm0n5t3r <bossm0n5t3r@gmail.com>
2023-06-25 04:07:30 +0900

    fix: Edit ResourceUtil
    - Use Paths

--- src/main/kotlin/redis/embedded/utils/ResourceUtil.kt
+++ src/main/kotlin/redis/embedded/utils/ResourceUtil.kt
@@ -2,10 +2,14 @@ package redis.embedded.utils
 
 import java.io.FileNotFoundException
 import java.net.URL
+import java.nio.file.Paths
 
 object ResourceUtil {
+    private val projectDirAbsolutePath = Paths.get("").toAbsolutePath().toString()
+    private val resourcesPath = Paths.get(projectDirAbsolutePath, "src/main/resources")
+
     fun getResource(path: String): URL {
-        return this.javaClass.classLoader.getResource(path)
+        return Paths.get(resourcesPath.toString(), path).toFile().toURI().toURL()
             ?: throw FileNotFoundException("resource $path not found")
     }
 }
