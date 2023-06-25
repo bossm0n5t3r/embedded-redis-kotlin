9c328c25f7b4b3093d50b150baf57fefaa40eba8
bossm0n5t3r <bossm0n5t3r@gmail.com>
2023-06-26 05:07:10 +0900

    fix: Edit RedisSentinelExecProvider
    - Edit initExecutables
      - Add OsArchitecture.UNIX_AMD64
      - Add OsArchitecture.UNIX_ARM64

--- src/main/kotlin/redis/embedded/RedisSentinelExecProvider.kt
+++ src/main/kotlin/redis/embedded/RedisSentinelExecProvider.kt
@@ -1,6 +1,8 @@
 package redis.embedded
 
-import redis.embedded.constants.RedisConstants.Architecture.ARCHITECTURE_ARM64
+import redis.embedded.constants.RedisConstants.Architecture.ARCHITECTURE_NAME_AMD64
+import redis.embedded.constants.RedisConstants.Architecture.ARCHITECTURE_NAME_ARM64
+import redis.embedded.constants.RedisConstants.OS.Name.OS_NAME_LINUX
 import redis.embedded.constants.RedisConstants.OS.Name.OS_NAME_MAC_OSX
 import redis.embedded.constants.RedisConstants.REDIS_VERSION
 import redis.embedded.constants.RedisConstants.Sentinel.REDIS_SENTINEL
@@ -18,12 +20,28 @@ class RedisSentinelExecProvider : RedisExecProvider() {
     }
 
     override fun initExecutables() {
+        executables[OsArchitecture.UNIX_AMD64] =
+            listOf(
+                REDIS_SENTINEL,
+                REDIS_VERSION,
+                OS_NAME_LINUX,
+                ARCHITECTURE_NAME_AMD64,
+            ).joinToString(HYPHEN)
+
+        executables[OsArchitecture.UNIX_ARM64] =
+            listOf(
+                REDIS_SENTINEL,
+                REDIS_VERSION,
+                OS_NAME_LINUX,
+                ARCHITECTURE_NAME_ARM64,
+            ).joinToString(HYPHEN)
+
         executables[OsArchitecture.MAC_OS_X_ARM64] =
             listOf(
                 REDIS_SENTINEL,
                 REDIS_VERSION,
                 OS_NAME_MAC_OSX,
-                ARCHITECTURE_ARM64,
+                ARCHITECTURE_NAME_ARM64,
             ).joinToString(HYPHEN)
     }
 }
