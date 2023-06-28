7d196843913bf3e586c2285203df1ed73a3468cd
bossm0n5t3r <bossm0n5t3r@gmail.com>
2023-06-29 04:20:30 +0900

    test: Edit RedisSentinelTest
    - Delete testSimpleOperationsAfterRun test

--- src/test/kotlin/redis/embedded/RedisSentinelTest.kt
+++ src/test/kotlin/redis/embedded/RedisSentinelTest.kt
@@ -1,14 +1,8 @@
 package redis.embedded
 
-import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Assertions.assertNull
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.Timeout
-import redis.clients.jedis.Jedis
-import redis.clients.jedis.JedisSentinelPool
-import redis.embedded.constants.RedisConstants.LOCALHOST
-import redis.embedded.constants.RedisConstants.Sentinel.DEFAULT_MASTER_NAME
 import redis.embedded.utils.generateRandomPort
 import java.util.concurrent.TimeUnit
 
@@ -47,32 +41,5 @@ class RedisSentinelTest {
         sentinel.stop()
         sentinel.start()
         sentinel.stop()
-    }
-
-    @Test
-    fun testSimpleOperationsAfterRun() {
-        // given
-        val redisServer = RedisServer(masterPort)
-        redisServer.start()
-        sentinel.start()
-        TimeUnit.SECONDS.sleep(1)
-
-        // when
-        var pool: JedisSentinelPool? = null
-        var jedis: Jedis? = null
-        try {
-            pool = JedisSentinelPool(DEFAULT_MASTER_NAME, setOf("$LOCALHOST:$sentinelPort"))
-            jedis = pool.resource
-            jedis.mset("abc", "1", "def", "2")
-
-            // then
-            assertEquals("1", jedis.mget("abc")[0])
-            assertEquals("2", jedis.mget("def")[0])
-            assertNull(jedis.mget("xyz")[0])
-        } finally {
-            if (jedis != null) pool?.returnResource(jedis)
-            sentinel.stop()
-            redisServer.stop()
-        }
     }
 }
