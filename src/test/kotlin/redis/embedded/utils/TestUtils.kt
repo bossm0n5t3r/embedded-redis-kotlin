7378d09d42685d62397152128801df37ec216a6b
bossm0n5t3r <bossm0n5t3r@gmail.com>
2023-06-28 23:50:00 +0900

    test: Edit TestUtils.kt
    - Add generateRandomString

--- src/test/kotlin/redis/embedded/utils/TestUtils.kt
+++ src/test/kotlin/redis/embedded/utils/TestUtils.kt
@@ -3,3 +3,14 @@ package redis.embedded.utils
 import kotlin.random.Random
 
 fun generateRandomPort() = Random.nextInt(0, 65535)
+
+private val CHAR_RANGE = 'a'..'z'
+
+fun generateRandomString(minLengthInclusive: Int, maxLengthExclusive: Int): String {
+    val length = Random.nextInt(minLengthInclusive, maxLengthExclusive)
+    return StringBuilder(length).apply {
+        repeat(length) {
+            this.append(CHAR_RANGE.random())
+        }
+    }.toString()
+}
