830fa8f2b792ac7a8cf280a0d5ede5a996158a6e
bossm0n5t3r <bossm0n5t3r@gmail.com>
2023-07-01 04:23:03 +0900

    fix: Edit JedisUtil
    - Delete unused methods

--- src/main/kotlin/redis/embedded/utils/JedisUtil.kt
+++ src/main/kotlin/redis/embedded/utils/JedisUtil.kt
@@ -1,27 +1,15 @@
 package redis.embedded.utils
 
-import redis.embedded.Redis
-import redis.embedded.RedisCluster
 import redis.embedded.RedisSentinel
 import redis.embedded.constants.RedisConstants.LOCALHOST
 
 object JedisUtil {
-    fun jedisHosts(redis: Redis): Set<String> {
-        val ports = redis.ports()
-        return portsToJedisHosts(ports)
-    }
-
-    fun sentinelHosts(cluster: RedisCluster): Set<String> {
-        val ports = cluster.sentinelPorts()
-        return portsToJedisHosts(ports)
-    }
-
     fun sentinelJedisHosts(redisSentinel: RedisSentinel): Set<String> {
         val ports = redisSentinel.ports()
         return portsToJedisHosts(ports)
     }
 
-    fun portsToJedisHosts(ports: Set<Int>): Set<String> {
+    private fun portsToJedisHosts(ports: Set<Int>): Set<String> {
         return ports.map { "$LOCALHOST:$it" }.toSet()
     }
 }
